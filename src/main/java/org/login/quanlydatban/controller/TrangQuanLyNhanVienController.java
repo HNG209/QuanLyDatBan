3b066c645d693a826ad87c35cefd7581af714ead
Co Phuong Hang <Dungheongunhubo>
2024-10-30 20:01:45 +0700

    j

--- src/main/java/org/login/quanlydatban/controller/TrangQuanLyNhanVienController.java
+++ src/main/java/org/login/quanlydatban/controller/TrangQuanLyNhanVienController.java
@@ -4,40 +4,60 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class TrangQuanLyNhanVienController {
+public class TrangQuanLyNhanVienController implements Initializable {
     @FXML
     private Button btnTaiAnh;
+    @FXML
+    private ImageView image1;
+    private String duongdananh;
 
     @FXML
     public  void taiAnh(){
+        btnTaiAnh = new Button();
         btnTaiAnh.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                JFileChooser fileChooser = new JFileChooser();
-                fileChooser.setDialogTitle("Chọn ảnh");
-                fileChooser.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("Image Files", "jpg"));
-                // Mở hộp thoại và chờ người dùng chọn file
-                int userSelection = fileChooser.showOpenDialog(null);
+                System.out.println("Nhan nut tai anh");
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setInitialDirectory(new File("E:\\QuanLyDatBanNhom2\\QuanLyDatBan\\src\\main\\resources\\org\\login\\quanlydatban\\Image"));
+                fileChooser.setTitle("Mở file");
 
-                // Kiểm tra xem người dùng đã chọn file chưa
-                if (userSelection == JFileChooser.APPROVE_OPTION) {
-                    File fileToOpen = fileChooser.getSelectedFile();
-                    String filePath = fileToOpen.getAbsolutePath(); // Lấy đường dẫn file
+                // Thiết lập bộ lọc file nếu cần
+                FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif");
+                fileChooser.getExtensionFilters().add(extFilter);
 
-                    // Thêm đường dẫn vào JComboBox
+                File file = fileChooser.showOpenDialog(null);
+                System.out.println("Nhấn nút tải ảnh");
 
+                if (file != null) {
+                    duongdananh = file.getAbsolutePath(); // Cập nhật đường dẫn
+                    // Cập nhật ImageView với ảnh mới
+                    Image image = new Image(file.toURI().toString());
+                    image1.setImage(image);
                 }
+
             }
         });
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
 
