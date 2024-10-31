146ceaf3371300227b171ecccac62c6ba15334da
Hung ne <hungok2909@gmail.com>
2024-10-31 12:13:00 +0700

    load ban's data and add image hover effect

--- src/main/java/org/login/quanlydatban/controller/ChonBanController.java
+++ src/main/java/org/login/quanlydatban/controller/ChonBanController.java
@@ -8,9 +8,12 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import org.login.quanlydatban.dao.BanDAO;
+import org.login.quanlydatban.entity.Ban;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ChonBanController implements Initializable {
@@ -27,13 +30,16 @@ public class ChonBanController implements Initializable {
         flowPane.prefHeightProperty().bind(scrollPane.heightProperty());
         flowPane.prefWidthProperty().bind(scrollPane.widthProperty());
 
-        for (int i = 0; i < 10; i++){
+        BanDAO banDAO = new BanDAO();
+        List<Ban> list = banDAO.readAll();
+
+        for (Ban i : list){
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/login/quanlydatban/uicomponents/CardBan.fxml"));
             try {
                 AnchorPane pane = loader.load();
                 CardBanController controller = loader.getController();
 
-                controller.setIndex(i);
+                controller.setBan(i);
 
                 flowPane.getChildren().add(pane);
             } catch (IOException e) {
