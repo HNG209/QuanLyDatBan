bd4e823165c415866873070c3083626fc3dd0e82
Hung ne <hungok2909@gmail.com>
2024-11-04 00:47:14 +0700

    add giuBan and xntt button

--- src/main/java/org/login/quanlydatban/controller/DatMonController.java
+++ src/main/java/org/login/quanlydatban/controller/DatMonController.java
@@ -1,21 +1,26 @@
 package org.login.quanlydatban.controller;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import org.login.quanlydatban.dao.BanDAO;
 import org.login.quanlydatban.dao.MonAnDAO;
 import org.login.quanlydatban.entity.Ban;
 import org.login.quanlydatban.entity.MonAn;
+import org.login.quanlydatban.entity.enums.TrangThaiBan;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class DatMonController implements Initializable {
@@ -26,20 +31,57 @@ public class DatMonController implements Initializable {
     private ScrollPane scrollPane;
 
     @FXML
-    private TableView<?> orderTable;
-
-    @FXML
     private AnchorPane anchorPane;
 
-    private MonAnDAO monAnDAO;
+    @FXML
+    private TextField banID;
 
     @FXML
-    private Label tableName;
+    private TextField trangThaiBanText;
+
+    @FXML
+    private Button btnGiuBan;
+
+    @FXML
+    private Button btnHuy;
+    private MonAnDAO monAnDAO;
+
+    private BanDAO banDAO;
+
+    @FXML
+    private TableView<Objects> orderTable;
+
+    @FXML
+    private TableColumn<Objects, Double> donGia;
+
+    @FXML
+    private TableColumn<Objects, String> dvt;
+
+    @FXML
+    private TableColumn<Objects, String> ghiChu;
+
+    @FXML
+    private TableColumn<Objects, String> maBan;
+
+    @FXML
+    private TableColumn<Objects, Integer> soLuong;
+
+    @FXML
+    private TableColumn<Objects, String> tenMon;
+
+    private ObservableList<Objects> objectsObservableList;
+
     private Ban ban;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         monAnDAO = new MonAnDAO();
+        banDAO = new BanDAO();
+
+        objectsObservableList = FXCollections.observableArrayList();
+
+//        donGia.cellValueFactoryProperty(data -> new SimpleDoubleProperty())
+
         monAnDAO.readAll();
         flowPane.prefHeightProperty().bind(scrollPane.heightProperty());
         flowPane.prefWidthProperty().bind(scrollPane.widthProperty());
@@ -50,7 +92,7 @@ public class DatMonController implements Initializable {
                 AnchorPane pane = loader.load();
 
                 CardMonAnController controller = loader.getController();
-                controller.setMonAn(i);
+                controller.setMonAn(i, this);
 
                 flowPane.getChildren().add(pane);
             } catch (IOException e) {
@@ -76,9 +118,37 @@ public class DatMonController implements Initializable {
 
     public void setBan(Ban ban) {
         this.ban = ban;
-        tableName.setText(ban.getMaBan());
+        this.banID.setText(ban.getMaBan());
+        this.trangThaiBanText.setText(ban.getTrangThaiBan().toString());
+
+        if(ban.getTrangThaiBan().equals(TrangThaiBan.BAN_TRONG)) {
+            this.btnHuy.setVisible(false);
+            this.btnGiuBan.setVisible(true);
+        }
+        else {
+            this.btnHuy.setVisible(true);
+            this.btnGiuBan.setVisible(false);
+        }
     }
 
+    public Ban getBan() {
+        return this.ban;
+    }
+
+    @FXML
+    void giuBan(ActionEvent event) {
+        this.ban = banDAO.updateBan(ban, TrangThaiBan.DANG_PHUC_VU);
+        this.trangThaiBanText.setText(ban.getTrangThaiBan().toString());
+        this.btnHuy.setVisible(true);
+        this.btnGiuBan.setVisible(false);
+    }
+    @FXML
+    void huyBan(ActionEvent event) {
+        this.ban = banDAO.updateBan(ban, TrangThaiBan.BAN_TRONG);
+        this.trangThaiBanText.setText(ban.getTrangThaiBan().toString());
+        this.btnHuy.setVisible(false);
+        this.btnGiuBan.setVisible(true);
+    }
     @FXML
     void back(MouseEvent event) throws IOException {
         if(anchorPane.getParent() instanceof BorderPane){
