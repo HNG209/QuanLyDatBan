c821344455d53b973a886a10c0dd33ca7442cba1
Hung ne <hungok2909@gmail.com>
2024-11-08 21:52:22 +0700

    logout added and handled

--- src/main/java/org/login/quanlydatban/controller/TrangChuController.java
+++ src/main/java/org/login/quanlydatban/controller/TrangChuController.java
@@ -2,14 +2,27 @@ package org.login.quanlydatban.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
+import javafx.scene.Scene;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.login.quanlydatban.entity.TaiKhoan;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class TrangChuController {
+public class TrangChuController implements Initializable {
     @FXML
     private Label tenNhanVien;
 
@@ -21,6 +34,8 @@ public class TrangChuController {
     @FXML
     private BorderPane borderPane;
 
+    @FXML
+    private ImageView setting;
 
     public void setTaiKhoan(TaiKhoan taiKhoan){
         this.taiKhoan = taiKhoan;
@@ -98,5 +113,52 @@ public class TrangChuController {
             System.err.println("Không thể tải giao diện Hoa Đơn: " + e.getMessage());
             e.printStackTrace();
         }
+    }
+
+    public void dangXuat(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/login/quanlydatban/views/TrangDangNhap.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage = new Stage();
+
+        if (stage != null){
+            stage.setTitle("Đăng nhập");
+            stage.setScene(scene);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(scene);
+            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/org/login/quanlydatban/stylesheets/style.css")).toExternalForm());
+            stage.show();
+        }
+
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ContextMenu contextMenu = new ContextMenu();
+
+        // Add MenuItems to the ContextMenu
+        MenuItem item1 = new MenuItem("Tài khoản");
+        MenuItem item2 = new MenuItem("Option 2");
+        SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
+        MenuItem item3 = new MenuItem("Đăng xuất");
+        item3.setOnAction(event -> {
+            Stage stage = (Stage) setting.getScene().getWindow();
+            stage.close();
+            try {
+                dangXuat(stage);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        contextMenu.getItems().addAll(item1, item2, separatorMenuItem, item3);
+
+
+        setting.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+                Bounds boundsInScreen = setting.localToScreen(setting.getBoundsInLocal());
+                contextMenu.show(setting, boundsInScreen.getMinX() - 30, boundsInScreen.getMaxY() + 5);
+            }
+        });
     }
 }
